eba97241192f24309f035b4aa9f1df937af89ea0
AjayPrasadVerma <imajvermaofficial@gmail.com>
2024-08-24 22:48:51 +0530

    Navigation

---
+++ components/main-header.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import classes from "./main-headee.module.css";
+import logoImg from "@/assets/logo.png";
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <Image src={logoImg} priority alt="A plate with food on it" />
+        NextLevel Food
+      </Link>
+
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
