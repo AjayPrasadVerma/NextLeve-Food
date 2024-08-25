866a463c25dcff1434752ed064754049c7e58a3e
AjayPrasadVerma <imajvermaofficial@gmail.com>
2024-08-25 13:27:25 +0530

    meals featching and loader added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-const MealsPage = () => {
-  return <h1>MealsPage</h1>;
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals.grid";
+import { getMeals } from "@/lib/meals";
+
+const MealsPage = async () => {
+  const meals = await getMeals();
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
