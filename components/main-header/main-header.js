0bd310aa5855008b25a3ab9e5b649b46bee75625
AjayPrasadVerma <imajvermaofficial@gmail.com>
2024-08-25 10:57:36 +0530

    home page

---
+++ components/main-header/main-header.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import classes from "./main-headee.module.css";
+import logoImg from "@/assets/logo.png";
+import MainHeaderBackground from "./main-header-background";
+
+const MainHeader = () => {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} priority alt="A plate with food on it" />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default MainHeader;
