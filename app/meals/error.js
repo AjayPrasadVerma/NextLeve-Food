72e93f288828bf3e59689668a946933a0bc3c690
AjayPrasadVerma <imajvermaofficial@gmail.com>
2024-08-25 15:38:32 +0530

    not-found or error page added

---
+++ app/meals/error.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>An errro occured!</h1>
+      <p>Failed to fetch meal data. Please try later.</p>
+    </main>
+  );
+};
+
+export default Error;
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals.grid";
 import { getMeals } from "@/lib/meals";
+import MealsLoadingPage from "./loading-out";
 
-const MealsPage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
 
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +25,9 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
